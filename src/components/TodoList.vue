4a396b743962d2cba216ad95d44f8c5d20f36c6b
Pierre Cordier <pierre.cordier@effisciency.com>
2021-01-19 01:28:39 +0100

    Simplify todo item

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="q-pt-xl q-mt-xl col-xs-12 col-sm-6 col-md-4">
-    <div></div>
-    <div class="title text-center q-pt-xl q-mt-xl">
-      <span class="text-weight-medium">todo</span>toudou.
-    </div>
+  <div class="text-center q-pt-lg q-mt-lg">
     <div>
-      <q-list separator>
+      <q-list>
         <todo-item
           v-for="(todo, key) in todos"
           :key="key"
@@ -17,21 +13,16 @@
       <q-item v-if="todos.length === 0" class="flex-center">
         <h5 class="text-grey-8">No todotoudous</h5>
       </q-item>
-      <new-todo-item />
     </div>
   </div>
 </template>
 
 <script>
-import TodoItemVue from "./TodoItem.vue";
-import NewTodoItemVue from "./NewTodoItem.vue";
+import TodoItem from "./TodoItem.vue";
 
 export default {
-  name: "Todos",
-  components: {
-    "todo-item": TodoItemVue,
-    "new-todo-item": NewTodoItemVue,
-  },
+  name: "TodoList",
+  components: { TodoItem },
   data() {
     return {};
   },
@@ -52,8 +43,4 @@ export default {
 </script>
 
 <style scoped>
-.title {
-  color: white;
-  font-size: 2em;
-}
 </style>
